b5dd4845568d32d4b6899dcee094275136e8e6a2
Ben Pluim <bpluim@Administrators-MacBook-Pro.local>
2020-12-17 16:44:32 -0700

    cart styling

--- src/components/CartContents/index.js
+++ src/components/CartContents/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import CartContext from 'context/CartContext';
-import {CartItem} from './styles';
+import {CartItem, CartHeader, CartFooter} from './styles';
 
 export function CartContents() {
     const {checkout} = React.useContext(CartContext);
@@ -8,6 +8,12 @@ export function CartContents() {
     return (
         <section>
             <h1>Your Cart</h1>
+            <CartHeader>
+                <div>Product</div>
+                <div>Unit Price</div>
+                <div>Quantity</div>
+                <div>Amount</div>
+            </CartHeader>
             {checkout?.lineItems?.map(item => {
                 return (
                     <CartItem key={item.variant.id}>
@@ -26,11 +32,15 @@ export function CartContents() {
                             {item.quantity}
                         </div>
                         <div>
-                            {(item.quantity * item.variant.price).toFixed(2)}
+                            ${(item.quantity * item.variant.price).toFixed(2)}
                         </div>
                     </CartItem>
                 );
             })}
+            <CartFooter>
+                <div />
+                <div><strong>Total: </strong>${checkout.totalPrice}</div>
+            </CartFooter>
         </section>
     )
 }
